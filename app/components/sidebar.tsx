68c52e67739456cd889222d854aac015be95d15e
bugsmith <173265808@qq.com>
2024-01-09 10:09:09 +0800

    fix

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           PuffChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          œ„œ„»Ì»Ì–°≈›‹Ω.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
